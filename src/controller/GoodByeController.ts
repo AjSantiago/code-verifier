687b323537ba05edf21d96713a0bd944ebdc8a49
AjSantiago <isc_aj.santiago@hotmail.com>
2022-11-29 00:08:33 -0600

    Webpack done & tsoa installed for Swagger

--- src/controller/GoodByeController.ts
+++ src/controller/GoodByeController.ts
@@ -1,10 +1,20 @@
+import { Get, Query, Route, Tags } from 'tsoa';
 import { iGoodByeController } from './interfaces';
 import { BasicDateResponse } from './types';
 import { LogSuccess } from '../utils/logger';
 
+@Route('/api/goodbye')
+@Tags('GoodByeController')
 export class GoodByeController implements iGoodByeController {
+  /**
+   * Endpointto retreive a Message 'GoodBye {name}, date' in JSON
+   * @param {string|undefined} name
+   * @param {Date} date
+   * @returns {BasicDateResponse} Promise of BasicDateResponse
+   */
+  @Get('/')
   public async getMessage(
-    name?: string | undefined,
+    @Query() name?: string | undefined,
     date?: Date | undefined
   ): Promise<BasicDateResponse> {
     LogSuccess('[/api/goodbye] Get Request');
--- src/controller/HelloController.ts
+++ src/controller/HelloController.ts
@@ -1,9 +1,20 @@
+import { Get, Query, Route, Tags } from 'tsoa';
 import { iHelloController } from './interfaces';
 import { BasicResponse } from './types';
 import { LogSuccess } from '../utils/logger';
 
+@Route('/api/hello')
+@Tags('HelloController')
 export class HelloController implements iHelloController {
-  public async getMessage(name?: string | undefined): Promise<BasicResponse> {
+  /**
+   * Endpointto retreive a Message 'Hello {name}' in JSON
+   * @param {string | undefined} name Name of user to be greeted
+   * @returns {BasicResponse} Promise of BasicResponse
+   */
+  @Get('/')
+  public async getMessage(
+    @Query() name?: string | undefined
+  ): Promise<BasicResponse> {
     LogSuccess('[/api/hello] Get Request');
 
     return {
